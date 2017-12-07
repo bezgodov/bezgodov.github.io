9b82e309e5d13b68001e3300ce4a1e9f00fb350b
bezgodov <maksim_bezgodov@mail.ru>
2017-12-07 17:28:56 +1000

    Added projects which weren't uploaded

---
+++ projects/mountains/assets/js/functions.js
@@ -0,0 +1,17 @@
+(function ($) {
+  'use strict';
+
+  $(document).ready(function () {
+    
+    $(document).ready(function(){
+        $('.header-menu a').click( function(){
+      var scroll_el = $(this).attr('href');
+            if ($(scroll_el).length != 0) { 
+          $('html, body').animate({ scrollTop: $(scroll_el).offset().top }, 500);
+            }
+          return false;
+        });
+    });
+  });
+
+}(jQuery));
